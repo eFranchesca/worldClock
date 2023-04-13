7eff8f3c44f1ffd1188f1637461bc4d1b68bd583
eFranchesca <112275326+eFranchesca@users.noreply.github.com>
2023-04-13 14:48:15 -0400

    added current time zone

--- javascript/index.js
+++ javascript/index.js
@@ -23,6 +23,9 @@ sanFranciscoTimeElement.innerHTML = `${sanFranciscoTime.format('h:mm:ss')}
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
